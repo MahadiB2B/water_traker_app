c90eb523394ecc3284251fd2a3d1a94815bdab73
Md Mahadi Hasan <mahadib2b@gmail.com>
2024-09-24 20:04:32 +0600

    waterTrack App

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -8,10 +8,43 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+ final TextEditingController  _glassNoTEController = TextEditingController(text:'1');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Water-Traker App"),
       centerTitle: true,),
+      body: Column(
+     crossAxisAlignment:CrossAxisAlignment.center,
+        children: [
+          const Text("12"),
+          const Text("Glasses"),
+          Row(
+            mainAxisAlignment:MainAxisAlignment.center,
+            children: [
+            SizedBox(
+              width: 50,
+              child: TextField(
+                controller: _glassNoTEController,
+                keyboardType: TextInputType.number,
+                textAlign: TextAlign.center,
+              ),
+            ),
+              TextButton(onPressed: (){},
+                  child: const Text("Add")),
+            ],
+          ),
+          const SizedBox(height: 16,),
+          Expanded(
+            child: ListView.separated(itemBuilder: (context,index){return ListTile(
+              title: const Text("Time"),
+              subtitle: const Text("Date"),
+              leading: const Text("1"),
+              trailing: IconButton(onPressed: (){}, icon: const Icon(Icons.delete)),
+            );},
+              separatorBuilder: (context,index){return const Divider();},
+              itemCount: 3),)
+        ],
+      ),
     );}
 }
